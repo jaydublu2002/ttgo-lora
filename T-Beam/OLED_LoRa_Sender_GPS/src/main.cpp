0662a5983992688d995841052c685d3c6f064708
Jim Whiteside <jaydublu2002@gmail.com>
2021-01-24 21:29:07 +0000

    Add Battery voltage - AXP20X library

--- T-Beam/OLED_LoRa_Sender_GPS/src/main.cpp
+++ T-Beam/OLED_LoRa_Sender_GPS/src/main.cpp
@@ -1,36 +1,41 @@
 /**
  * Derived from
  * https://github.com/LilyGO/TTGO-LORA32/blob/master/OLED_LoRa_Receive/OLED_LoRa_Receive.ino
- * with OLED pins amended to suit TTGO T-Beam V1.1
+ * with OLED & GPS pins amended to suit TTGO T-Beam V1.1
  * 
  * Combined with https://github.com/LilyGO/TTGO-T-Beam/blob/master/GPS/GPS.ino
  */
 
 #include <Arduino.h>
+#include <axp20x.h>
 #include <LoRa.h>
 #include <SPI.h>
-#include <SSD1306.h> 
-#include <TinyGPS++.h> 
-#include <Wire.h>  
+#include <SSD1306.h>
+#include <TinyGPS++.h>
+#include <Wire.h>
 
-#define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISnO
-#define MOSI    27   // GPIO27 -- SX1278's MOSI
-#define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
-#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND  868E6
+#define SCK 5   // GPIO5  -- SX1278's SCK
+#define MISO 19 // GPIO19 -- SX1278's MISnO
+#define MOSI 27 // GPIO27 -- SX1278's MOSI
+#define SS 18   // GPIO18 -- SX1278's CS
+#define RST 14  // GPIO14 -- SX1278's RESET
+#define DI0 26  // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND 868E6
+
+const uint8_t i2c_sda = 21;
+const uint8_t i2c_scl = 22;
 
 unsigned int counter = 0;
 
-SSD1306 display(0x3c, 21, 22);
+AXP20X_Class axp;
+SSD1306 display(0x3c, i2c_sda, i2c_scl);
 String rssi = "RSSI --";
 String packSize = "--";
-String packet ;
-TinyGPSPlus gps;                            
-HardwareSerial SerialGPS(1);  
+String packet;
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(1);
 
-static void smartDelay(unsigned long ms)                
+static void smartDelay(unsigned long ms)
 {
   unsigned long start = millis();
   do
@@ -40,39 +45,55 @@ static void smartDelay(unsigned long ms)
   } while (millis() - start < ms);
 }
 
-void setup() {
-  pinMode(16,OUTPUT);
-  // T-Beam doesn't have a user LED? 
+void setup()
+{
+  pinMode(16, OUTPUT);
+  // T-Beam doesn't have a user LED?
   // pinMode(14,OUTPUT);
-  
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
-  delay(50); 
+
+  digitalWrite(16, LOW); // set GPIO16 low to reset OLED
+  delay(50);
   digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
-  
+
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println();
   Serial.println("LoRa Sender Test");
 
-  SerialGPS.begin(9600, SERIAL_8N1, 34, 12);   //17-TX 18-RX
-  
-  SPI.begin(SCK,MISO,MOSI,SS);
-  LoRa.setPins(SS,RST,DI0);
-  if (!LoRa.begin(868E6)) {
+  Wire.begin(i2c_sda, i2c_scl);
+
+  int ret = axp.begin(Wire, AXP192_SLAVE_ADDRESS);
+
+  if (ret == AXP_FAIL)
+  {
+    Serial.println("AXP Power begin failed");
+    while (1)
+      ;
+  }
+
+  SerialGPS.begin(9600, SERIAL_8N1, 34, 12); //17-TX 18-RX
+
+  SPI.begin(SCK, MISO, MOSI, SS);
+  LoRa.setPins(SS, RST, DI0);
+  if (!LoRa.begin(868E6))
+  {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
   //LoRa.onReceive(cbk);
-//  LoRa.receive();
+  //  LoRa.receive();
   Serial.println("init ok");
   display.init();
-  display.flipScreenVertically();  
+  display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
-   
+
   delay(1500);
 }
 
-void loop() {
+void loop()
+{
 
   Serial.print("Latitude  : ");
   Serial.println(gps.location.lat(), 5);
@@ -91,7 +112,12 @@ void loop() {
   Serial.println(gps.time.second());
   Serial.println("**********************");
 
-  smartDelay(1000);                                      
+  Serial.print("Battery: ");
+  Serial.print(String(axp.getBattVoltage()) + " mv");
+  Serial.print(axp.isChargeing() ? " - Charging" : " - CHARGED");
+  Serial.print("\r\n");
+
+  smartDelay(1000);
 
   if (millis() > 5000 && gps.charsProcessed() < 10)
     Serial.println(F("No GPS data received: check wiring"));
@@ -99,11 +125,12 @@ void loop() {
   display.clear();
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
-  
+
   display.drawString(0, 0, "Sending packet: ");
   display.drawString(90, 0, String(counter));
-  Serial.println(String(counter));
   display.display();
+
+  Serial.println(String(counter));
 
   // send packet
   LoRa.beginPacket();
@@ -115,8 +142,6 @@ void loop() {
   LoRa.endPacket();
 
   counter++;
-  // digitalWrite(14, HIGH);   // turn the LED on (HIGH is the voltage level)
-  // delay(1000);                       // wait for a second
-  // digitalWrite(14, LOW);    // turn the LED off by making the voltage LOW
-  delay(2000);                       // wait for a second
+
+  delay(2000);
 }
